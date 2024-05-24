6093d3f3e52c39861e9c395e0d407a3fdf1d6dc6
AndreiK <anjey1@gmail.com>
2024-05-24 17:32:06 +0300

    fix

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('ball licking pirate game')
+        pygame.display.set_caption('pirate game')
         self.screen = pygame.display.set_mode((640, 480))
 
         self.display = pygame.Surface((320, 240))
